ea1ea53c5d701ae4a03833dfd3138c83e8081b63
PrimePian <primepian52@gmail.com>
2025-01-15 11:00:30 -0600

    Con Mecanismos

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanismos/BarredoraTest.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode.mecanismos;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "Motor Test ")
+public class BarredoraTest extends LinearOpMode {
+    DcMotor motor;
+    public Servo Articulacion_Barredora1;
+    public Servo Articulacion_Barredora2;
+    DcMotor Barredora;
+    //     ticks = 537.7;
+    int newTarget = 90;
+    public int nsdkl(int degrees){return (int) Math.round(degrees * 1.4936111);}
+
+    public void runOpMode(){
+        initBarredora();
+        waitForStart();
+
+        while (opModeIsActive()) {
+            if (gamepad1.right_bumper) {
+                motor.setTargetPosition(nsdkl(newTarget));
+                motor.setPower(0.6);
+                sleep(1200);
+                Articulacion(0.5,0.5);
+            }
+            if (gamepad1.left_bumper) {
+                Articulacion(0.0,1.0);
+                sleep(1200);
+                motor.setTargetPosition(0);
+                motor.setPower(0.6);
+            }
+            telemetry.addData("motorPos", motor.getTargetPosition());
+
+            if (gamepad1.a){
+                Barredora.setPower(1.0);
+            } else if (gamepad1.x){
+                Barredora.setPower(-1.0);
+            } else{
+                Barredora.setPower(0.0);
+            }
+        }
+    }
+    public void initBarredora(){
+        motor = hardwareMap.get(DcMotor.class, "Corredera");
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor.setTargetPosition(nsdkl(newTarget));
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        Articulacion_Barredora1 = hardwareMap.get(Servo.class, "A_Barredora1");
+        Articulacion_Barredora2 = hardwareMap.get(Servo.class, "A_Barredora2");
+        Barredora = hardwareMap.get(DcMotor.class, "Barredora");
+        telemetry.addLine("Barredora iniciada");
+        telemetry.update();
+    }
+    public void Articulacion(double POS, double POS2){
+        Articulacion_Barredora1.setPosition(POS);
+        Articulacion_Barredora2.setPosition(POS2);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanismos/ElevadorTest1.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode.mecanismos; // ELEVADOR CON TRIGGERS
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class ElevadorTest1 extends LinearOpMode {
+    public DcMotor elevador1 = null;
+    public DcMotor elevador2 = null;
+//    private static final double TICKS_PER_REVOLUTION = 8192;   //537.7
+//    int elevatorTolerance = 50;
+    @Override
+    public void runOpMode() {
+        initElevador();
+        waitForStart();
+
+        while (opModeIsActive()){
+            float fr = gamepad1.right_trigger;
+            float fl = gamepad1.left_trigger;
+
+            String sr = Float.toString(fr);
+            String sl = Float.toString(fl);
+
+            double RightTriggerPos = Double.parseDouble(sr);
+            double LeftTriggerPos = Double.parseDouble(sl);
+
+            //ELEVADOR
+            if(gamepad1.right_trigger > 0){  //mover Elevador Arriba
+                elevadorEnfrente(RightTriggerPos);
+            }
+            if(gamepad1.left_trigger > 0){  //mover Elevador Abajo
+                elevadorAtras(LeftTriggerPos);
+            }
+            elevador1.setPower(0);
+            elevador2.setPower(0);
+        }
+    }
+
+
+    public void initElevador(){
+        elevador1 = hardwareMap.get(DcMotor.class,"elevador1");
+        elevador2 = hardwareMap.get(DcMotor.class,"elevador2");
+//        elevador1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        elevador2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        elevador1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        elevador2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        elevador2.setDirection(DcMotorSimple.Direction.REVERSE);
+        elevador1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        elevador2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        telemetry.addLine("Elevadores iniciados");
+    }
+    public void elevadorEnfrente(double POWER){
+        elevador1.setPower(POWER);
+        elevador2.setPower(POWER);
+    }
+    public void elevadorAtras(double POWER){
+        elevador1.setPower(-POWER);
+        elevador2.setPower(-POWER);
+    }
+}
